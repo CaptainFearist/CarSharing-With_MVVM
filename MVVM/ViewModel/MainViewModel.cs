0f338c6e71a03ccedff17f8ef780687f09175e9a
CaptainFearist <igorgolenisev1317@gmail.com>
2024-04-22 21:02:02 +0300

    Добавьте файлы проекта.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CarSharing.Core;
+using System;
+
+namespace CarSharing.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand CarsViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public CarsViewModel CarsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CarsVM = new CarsViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            CarsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = CarsVM;
+            });
+        }
+
+    }
+}
